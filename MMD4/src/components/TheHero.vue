ca14eec2c2e6a0e99e7973fff5e5e2f8b5647c24
NMaria92 <150144055+NMaria92@users.noreply.github.com>
2025-05-08 15:16:06 +0200

    added TheHero for the hero section component. tested in MotionView

---
+++ MMD4/src/components/TheHero.vue
@@ -0,0 +1,69 @@
+<script setup>
+defineProps({
+    title: String,
+    subtitle: String,
+    image: String,
+    alt: String
+});
+
+
+</script>
+
+<template>
+    <div class="hero">
+        <div class="hero-text">
+            <h1 class="hero-title">{{ title }}</h1>
+            <p class="hero-subtitle"> {{ subtitle }}</p>
+        </div>
+        <img :src="image" :alt="alt" id="hero-image" />
+    </div>
+</template>
+
+<style scoped>
+.hero {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 7.6rem;
+    /* max-height: 21.8rem; 
+    min-height: 7.6rem;  */
+    position: relative;
+    overflow: hidden;
+    background-color: rgb(20, 20, 20, 0.5);
+}
+
+.hero-text {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+}
+
+.hero-title {
+    font-size: clamp(3rem, 5vw, 5.33rem);
+    font-weight: 700;
+    text-transform: uppercase;
+    color: var(--color-font-2);
+    -webkit-text-stroke: 0.5px rgb(0, 0, 0);
+}
+
+.hero-subtitle {
+    font-size: clamp(1.5rem, 2vw, 3rem);
+    font-weight: regular;
+    color: var(--color-font-2);
+    -webkit-text-stroke: 0.5px rgb(0, 0, 0);
+}
+
+#hero-image {
+    width: 100%;
+    height: 7.6rem;
+    object-fit: cover;
+}
+</style>
--- MMD4/src/views/MotionView.vue
+++ MMD4/src/views/MotionView.vue
@@ -1,8 +1,16 @@
 <script setup>
+import { RouterView } from "vue-router";
+import TheHero from "../components/TheHero.vue";
+
 </script>
 
 <template>
-    <h2>MOTION</h2>
+    <TheHero
+        title="HARALDSLUND"
+        subtitle="Motion"
+        image="https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwzNjUyOXwwfDF8c2VhcmNofDJ8fG1vdGlvbnxlbnwwfHx8fDE2OTY5NTQ3NzE&ixlib=rb-4.0.3&q=80&w=1080"
+        alt="Motion"
+    />
 </template>
 
 <style scoped>
