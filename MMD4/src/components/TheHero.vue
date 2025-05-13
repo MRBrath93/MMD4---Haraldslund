952bae5cc70dd71e734bf21430660b5c2572df16
NMaria92 <150144055+NMaria92@users.noreply.github.com>
2025-05-13 15:06:38 +0200

    Added TheFooter component in App.vue
    Fixed CSS-BUG in TheHero

--- MMD4/src/components/TheHero.vue
+++ MMD4/src/components/TheHero.vue
@@ -12,8 +12,8 @@ defineProps({
 <template>
     <div class="hero">
         <div class="hero-text">
-            <h1 class="hero-title">{{ title }}</h1>
-            <p class="hero-subtitle"> {{ subtitle }}</p>
+            <h5 class="hero-title">{{ title }}</h5>
+            <h6 class="hero-subtitle"> {{ subtitle }}</h6>
         </div>
         <img :src="image" :alt="alt" id="hero-image" />
     </div>
@@ -21,58 +21,70 @@ defineProps({
 
 <style scoped>
 .hero {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    width: 100%;
-    height: 7.6rem;
-    /* max-height: 21.8rem; 
-    min-height: 7.6rem;  */
     position: relative;
     overflow: hidden;
-    background-color: rgb(20, 20, 20, 0.5);
-}
-
-.hero-text {
     display: flex;
-    flex-direction: column;
     justify-content: center;
     align-items: center;
     text-align: center;
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
+    height: 7.6rem;
 }
 
-.hero-title {
-    font-size: clamp(3rem, 5vw, 5.33rem);
+.hero-title{
+    font-size: clamp(2rem, 5vw, 4rem);
     font-weight: 700;
+    font-family: var( --font-heading);
     text-transform: uppercase;
-    color: var(--color-font-2);
-    -webkit-text-stroke: 0.5px rgb(0, 0, 0);
 }
 
-.hero-subtitle {
-    font-size: clamp(1.5rem, 2vw, 3rem);
-    font-weight: regular;
+.hero-subtitle{
+    font-size: clamp(1.25rem, 5vw, 2.25rem);
+    font-weight: 500;
+    font-family: var(--font-heading);
+}
+
+
+/* Overlay */
+.hero::before {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(20, 20, 20, 0.5);
+    z-index: 1;
+}
+
+.hero-text {
+    position: relative;
+    z-index: 2;
+}
+
+.hero-title, .hero-subtitle {
     color: var(--color-font-2);
-    -webkit-text-stroke: 0.5px rgb(0, 0, 0);
+    -webkit-text-stroke: 0.5px var(--color-font-1); 
+    -webkit-text-fill-color: var(--color-font-2);
 }
 
 #hero-image {
     width: 100%;
     height: 7.6rem;
     object-fit: cover;
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: 0;
 }
 
 @media screen and (min-width: 768px) {
-    .hero {
-        height: 21.8rem;
-    }
     #hero-image {
         height: 21.8rem;
     }
+
+    .hero {
+        height: 21.8rem;
+    }
 }
+
 </style>
